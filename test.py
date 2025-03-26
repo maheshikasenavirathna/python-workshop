53f0dd04d9718ec2838032743a1354d6e07da99e
unknown <239197@TECH.WYB.AC.LK>
2025-03-26 18:14:12 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,13 @@
+
+w=int(input("Enter first number"))
+y=int(input("enter second number"))
+p=w/y
+print(p)
+name="MAHESHIKA SENAVITRATHANA"
+print(name)
+print(name[8])
+print(name.lower())
+k="She is a student of WAYAMBA UNIVERSITY OF SRI LANKA"
+M=name+k
+print(k.lower)
+print(name[2:10])
