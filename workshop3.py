0f389fdaa03986262c484c483ce10b2edcc98894
unknown <239197@TECH.WYB.AC.LK>
2025-03-26 21:29:46 +0530

    first commit

---
+++ workshop3.py
@@ -0,0 +1,17 @@
+num=["saman","box",23,78,89]
+print(num[3])
+print(num[-3])
+print(type(num))
+for i in num:
+    print(type(i))
+wyb=[23,90,7,101,9,7]
+wyb.sort()
+print(wyb)
+wyb.append(88)
+print(wyb)
+print(wyb.index(7))
+print(max(wyb))
+print(min(wyb))
+print(len(wyb))
+wyb.clear()
+print(wyb)
